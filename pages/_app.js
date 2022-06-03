e31111b7943a5af293928a1d309f1537e01262ee
Joseph Dubon <josephdubon@pm.me>
2022-06-02 22:35:48 -0700

    added Head and bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/global.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Head from 'next/head'
+
+import MainLayout from '../components/layout/MainLayout'
+import {Layout} from 'antd'
+import {useEffect} from 'react'
+
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
