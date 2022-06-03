482769e90e6a47c55ffee0f20735030bbe580731
Joseph Dubon <josephdubon@pm.me>
2022-06-02 22:36:22 -0700

    added Head and bootstrap with MainLayout wrap

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,23 @@ import {Layout} from 'antd'
 import {useEffect} from 'react'
 
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}) {
+    // add this for bootstrap js components to render correctly
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap')
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <meta name='viewport' content='width=device-width, initial-scale=1'/>
+                <title>Mellow Slothz</title>
+            </Head>
+            <Layout className='layout'>
+                <MainLayout>
+                    <Component {...pageProps} />
+                </MainLayout>
+            </Layout>
+        </>
+    )
 }
