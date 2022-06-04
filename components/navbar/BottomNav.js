c592e3182877f74bdfaea6d2580d18a75435d436
Joseph Dubon <josephdubon@pm.me>
2022-06-03 22:23:17 -0700

    adjusted layout of bottom nav

--- components/navbar/BottomNav.js
+++ components/navbar/BottomNav.js
@@ -2,14 +2,13 @@ import Link from 'next/link'
 
 export default function BottomNav() {
     return (<>
-        <p className='lead text-black fw-bold text-center'>
+        <p className='lead text-black fw-bold'>
             <span
                 className='fw-bold small'>Read through our site and discover how <span
                 className='fw-bolder'>Mellow Slothz</span> will provide new opportunities in the near future.</span>
             <br/>
             <br/>
             <div className='small'>
-                {' | '}
                 <Link href='/information'><a>Information Page</a></Link>
                 {' | '}
                 <Link href='/information/#mission-statement'><a>Mission
@@ -17,13 +16,12 @@ export default function BottomNav() {
                 {' | '}
                 <Link href='/information/#who-we-donate-to'><a>Who We Donate
                     To?</a></Link>
-                {' | '}
+                <br/>
                 <Link href='/information/#future-utility'><a>Future Utility</a></Link>
                 {' | '}
                 <Link href='/information/#mellow-slothz-categories'><a>Mellow Slothz Categories</a></Link>
                 {' | '}
                 <Link href='/adoption-papers'><a>Adoption Papers</a></Link>
-                {' | '}
             </div>
         </p>
     </>)
