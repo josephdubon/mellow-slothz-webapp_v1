46d5ba225906e30e35a4fe9230349749f4989602
Joseph Dubon <josephdubon@pm.me>
2022-06-03 22:02:22 -0700

    single quotes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function IndexMain() {
         {/* page head */}
         <Head>
             <title>Mellow Slothz</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
         </Head>
 
         {/* content */}
