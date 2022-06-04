ead94abf335c160f47f7596ff85173bb6bcd26ee
Joseph Dubon <josephdubon@pm.me>
2022-06-03 20:40:05 -0700

    new sub headings

--- pages/index.js
+++ pages/index.js
@@ -46,11 +46,11 @@ export default function IndexMain() {
                             </div>
                         </div>
 
-                        <div className='d-md-flex flex-md-equal w-100 my-md-3 ps-md-3'>
+                        <div id='mellow-slothz-art' className='d-md-flex flex-md-equal w-100 my-md-3 ps-md-3'>
                             <div className='bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden'>
                                 <div className='my-3 p-3'>
                                     <h2 className='display-5'>Sample Art</h2>
-                                    <p className='lead'>And an even wittier subheading.</p>
+                                    <p className='lead'>Something about sample art here.</p>
                                 </div>
                                 <div className='bg-dark shadow-sm mx-auto'
                                      style={{width: '80%', height: '300px', borderRadius: '21px 21px 0 0'}}>{''}</div>
@@ -59,7 +59,7 @@ export default function IndexMain() {
                                 className='bg-blue me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden'>
                                 <div className='my-3 py-3'>
                                     <h2 className='display-5 text-white'>Base Art</h2>
-                                    <p className='lead text-white-50'>And an even wittier subheading.</p>
+                                    <p className='lead text-white-50'>Something about base art here.</p>
                                 </div>
                                 <div className='bg-light shadow-sm mx-auto'
                                      style={{width: '80%', height: '300px', borderRadius: '21px 21px 0 0'}}>{''}</div>
