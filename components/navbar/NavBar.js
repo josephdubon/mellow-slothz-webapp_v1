1b179689a79f59c82f32f81f1a4f134351e4cd6e
Joseph Dubon <josephdubon@pm.me>
2022-06-03 20:44:44 -0700

    updated keys and icons for nav items

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -21,69 +21,63 @@ export default function NavBar() {
     }, [isServer() && window.location.pathname])
 
     return (<>
-        <Menu
-            theme='dark'
-            mode='horizontal'
-            selectedKeys={[currentPage]}
-            defaultSelectedKeys={[currentPage]}
-            items={new Array(15).fill(null).map((_, index) => {
-                const key = index + 1
-                return {
-                    key,
-                    label: `nav ${key}`,
-                }
-            })}
-            className='mb-2'
-        >
-            {/* nav items */}
-            <Item
-                key='/'
-                onClick={(e) => {
-                    setCurrentPage(e.key)
-                }}
-                icon={<AppstoreOutlined/>}
-            >
-                <Link href='/'>
-                    <a>Home</a>
-                </Link>
-            </Item>
+        <div className='d-flex justify-content-start'>
+            <Header className='header'>
+                <Link href='/'><a>
+                    <div className='logo'>Mellow Slothz</div>
+                </a></Link>
+                <Menu
+                    theme='dark'
+                    mode='horizontal'
+                    selectedKeys={[currentPage]}
+                    defaultSelectedKeys={[currentPage]}
+                    items={new Array(15).fill(null).map((_, index) => {
+                        const key = index + 1
+                        return {
+                            key,
+                            label: `nav ${key}`,
+                        }
+                    })}
+                    className='mb-2'
+                >
+                    {/* nav items */}
+                    <Item
+                        key='/'
+                        onClick={(e) => {
+                            setCurrentPage(e.key)
+                        }}
+                        icon={<HomeFilled/>}
+                    >
+                        <Link href='/'>
+                            <a>Home</a>
+                        </Link>
+                    </Item>
 
-            <Item
-                key='/adoption'
-                icon={<TeamOutlined/>}
-                onClick={(e) => {
-                    setCurrentPage(e.key)
-                }}
-            >
-                <Link href='/adoption'>
-                    <a>Adoption</a>
-                </Link>
-            </Item>
+                    <Item
+                        key='/information'
+                        icon={<InfoCircleFilled/>}
+                        onClick={(e) => {
+                            setCurrentPage(e.key)
+                        }}
+                    >
+                        <Link href='/information'>
+                            <a>Information</a>
+                        </Link>
+                    </Item>
 
-            <Item
-                key='/information'
-                icon={<TeamOutlined/>}
-                onClick={(e) => {
-                    setCurrentPage(e.key)
-                }}
-            >
-                <Link href='/information'>
-                    <a>Information</a>
-                </Link>
-            </Item>
-
-            <Item
-                key='/contact'
-                icon={<TeamOutlined/>}
-                onClick={(e) => {
-                    setCurrentPage(e.key)
-                }}
-            >
-                <Link href='/contact'>
-                    <a>Contact</a>
-                </Link>
-            </Item>
-
-        </Menu>
+                    <Item
+                        key='/adoption-papers'
+                        icon={<CodeFilled/>}
+                        onClick={(e) => {
+                            setCurrentPage(e.key)
+                        }}
+                    >
+                        <Link href='/adoption-papers'>
+                            <a>Adoption Papers</a>
+                        </Link>
+                    </Item>
+                </Menu>
+            </Header>
+        </div>
     </>)
 }
