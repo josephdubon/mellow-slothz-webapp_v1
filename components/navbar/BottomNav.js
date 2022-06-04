81626a829266768f9e701917925f603ee5146715
Joseph Dubon <josephdubon@pm.me>
2022-06-03 20:47:55 -0700

    created new component for the bottom nav on the home page and the information page

---
+++ components/navbar/BottomNav.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+export default function BottomNav() {
+    return (<>
+        <p className='lead text-black fw-bold text-center'>
+            <span
+                className='fw-bold'>Read through our site and discover how <span
+                className='fw-bolder'>Mellow Slothz</span> will provide new opportunities in the near future.</span>
+            <br/>
+            <br/>
+            <div className='lead fw-bold'>
+                {' | '}
+                <Link href='/information'><a>Information Page</a></Link>
+                {' | '}
+                <Link href='/information/#mission-statement'><a>Mission
+                    Statement</a></Link>
+                {' | '}
+                <Link href='/information/#who-we-donate-to'><a>Who We Donate
+                    To?</a></Link>
+                {' | '}
+                <Link href='/information/#future-utility'><a>Future Utility</a></Link>
+                {' | '}
+                <Link href='/information/#mellow-slothz-categories'><a>Mellow Slothz Categories</a></Link>
+                {' | '}
+                <Link href='/adoption-papers'><a>Adoption Papers</a></Link>
+                {' | '}
+            </div>
+        </p>
+    </>)
+}
