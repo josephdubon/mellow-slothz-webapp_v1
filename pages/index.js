6c4d08f7bcbd334855446fa9bcc47f384f0de0cd
Joseph Dubon <josephdubon@pm.me>
2022-06-03 20:38:21 -0700

    recreated the 1st panel of the home page

--- pages/index.js
+++ pages/index.js
@@ -6,61 +6,33 @@ import BottomNav from '../components/navbar/BottomNav'
 
 const {Header, Content, Footer} = Layout
 
-export default function () {
+export default function IndexMain() {
     return (<>
         <Row className='bg-dark2 text-dark'>
             <Col xs={24} className='text-center'>
                 <Content>
-                    <main>
-                        <div className='position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center text-white-50'>
-                            <Row>
-                                <Col sm={24} md={24} lg={12} className='p-lg-5 mx-auto my-5 text-left'>
-                                    <h1 className='display-1 fw-normal fw-bolder text-white'>Mellow Slothz</h1>
-                                    <h4 className='display-6 fw-normal fw-bold text-white'>smooth, refined, and set to
-                                        HODL</h4>
-                                    <p className='lead fw-normal'>
-                                        We are Mellow Slothz…
-                                        <br/>
-                                        <br/>
-                                        Mellow Slothz is a CNFT project built on Cardano, home to some of the most
-                                        creative cNFT projects in the crypto space.
-                                    </p>
-                                    <p className='lead fw-normal'>
-                                        The idea for MS came from the idea that being successful in crypto means staying
-                                        calm, thinking it through, and not reacting like a nub when the market changes
-                                        suddenly.
-                                    </p>
-                                    <p className='lead fw-bold'>
-                                        Mellow. Calm. Purposeful.
-                                    </p>
-                                    <p className='lead fw-normal'>
-                                        A Sloth acts the same way most of the time.
-                                        <br/>
-                                        <br/>
-                                        A Sloth is also very strong. Hanging from trees all day will do that. Those
-                                        claws will grip hard and not let go without a struggle.
-                                    </p>
-                                    <p className='lead fw-normal'>
-                                        If you are into Cardano, you likely have a hard time letting go as well. The
-                                        best real life projects are being built and funded on Cardano.
-                                    </p>
-                                    <p className='lead fw-normal'>
-                                        Mellow Slothz is a constant reminder of the resilience we have against
-                                        negativity and FUD, and just how lucky we are to be here.
-                                    </p>
-                                    <a className='btn btn-outline-secondary' href='#'>Coming soon</a>
-                                </Col>
-                                <Col className='col-md-5 p-lg-5 mx-auto my-5 col-sm'>
-                                    <Image src='/images/Mellow_Slothz_Base.png' alt='me' width='800' height='800'/>
-                                    <p className='small'>
-                                        Keep return for updates and previews. We will update this page as more
-                                        becomes available.
-                                    </p>
-                                    <p className='small'>
-                                        Twitter, Discord, and Reddit links will be posted soon.
-                                    </p>
-                                </Col>
-                            </Row>
+                    <main className='wrapper'>
+                        <div
+                            className='d-md-flex flex-md-equal justify-content-center align-items-center w-100 my-md-3 ps-md-3'>
+                            <div className='p-lg-5 mx-auto my-5 text-white-50 text-center'>
+                                <h1 className='display-1 fw-normal fw-bolder text-white'>Mellow Slothz</h1>
+                                <h4 className='display-6 fw-normal fw-bold text-white'>smooth, refined, and set to
+                                    HODL</h4>
+                                <br/>
+                                <p className='lead fw-bold'>
+                                    We are Mellow Slothz… mellow. calm. purposeful.
+                                </p>
+                                <p className='lead fw-normal'>
+                                    We are an NFT project built on the Cardano Blockchain.
+                                    <br/>
+                                    Home to some of the most creative cNFT
+                                    projects in the crypto space.
+                                </p>
+                                <a className='btn btn-outline-light text-capitalize' href='#mellow-slothz-art'>Check out
+                                    our art</a>{' | | '}
+                                <a className='btn btn-outline-info text-capitalize' href='#why-are-we-doing-this'>Why are we doing
+                                    this?</a>
+                            </div>
 
                         </div>
 
