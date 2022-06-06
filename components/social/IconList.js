d11248eec0cf155e3fcca33639105dc6ed25ab2f
Joseph Dubon <josephdubon@pm.me>
2022-06-06 10:33:30 -0700

    removed Link from a tags since linking goes outside app

--- components/social/IconList.js
+++ components/social/IconList.js
@@ -1,21 +1,20 @@
-import Link from 'next/link'
 import {FaDiscord, FaReddit, FaTwitter} from 'react-icons/fa'
 import React from 'react'
 
 export default function IconList() {
     return (<>
         <div className='d-flex justify-content-center'>
-            <Link href='#'><a>
+            <a href='https://twitter.com/mellowslothz' target='_blank'>
                 <FaTwitter className='icon-list' size='2rem'/>
-            </a></Link>
+            </a>
 
-            <Link href='#'><a>
+            <a href='https://www.reddit.com/r/MellowSlothz/' target='_blank'>
                 <FaReddit className='icon-list' size='2rem'/>
-            </a></Link>
+            </a>
 
-            <Link href='#'><a>
+            <a href='#'>
                 <FaDiscord className='icon-list' size='2rem'/>
-            </a></Link>
+            </a>
         </div>
     </>)
 }
