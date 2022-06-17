427a374229add877854a3ec1622121ec02cec109
Joseph Dubon <josephdubon@pm.me>
2022-06-17 08:34:23 -0700

    added cards with radial gradient to sections

--- pages/information.js
+++ pages/information.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Col, Divider, Layout, List, Row} from 'antd'
+import {Card, Col, Divider, Layout, List, Row} from 'antd'
 import BottomNav from '../components/navbar/BottomNav'
 import Head from 'next/head'
 
@@ -50,84 +50,121 @@ export default function informationMain() {
                                 <div className='my-3 p-3'>
                                     <h2 className='display-5 text-white text-capitalize'>{''}</h2>
                                 </div>
-                                <div className='shadow-sm mx-auto p-3'>
 
-                                    {/* mission statement */}
-                                    <div id='mission-statement'>
-                                        <h1 className='display-5 text-white text-capitalize'>Mission Statement</h1>
-                                        <Divider/>
-                                        <p className='lead text-white-50 fw-normal text-left'>
-                                            Mellow Slothz is here to provide art when available, value when possible,
-                                            and opportunity where it can be utilized. We aim to always be fair and as
-                                            open as possible when the community needs us to be so that Mellow Slothz is
-                                            considered a respected member within the Cardano NFT community. We strive to
-                                            support our small stake pool operators as much as we are able in order to
-                                            promote healthy growth and decentralization on the network.
-                                            <br/>
-                                            <br/>
-                                            Mellow Slothz is also committed to doing what we are able to with respect to
-                                            the vulnerable sloth population. We will be donating at regular intervals to
-                                            The Sloth Conservation Foundation in Costa Rica. We will continue to look
-                                            for new missions to support as funds permit.
-                                        </p>
-                                        <br/>
-                                        <br/>
-                                    </div>
+                                <Row gutter={[16, 24]}>
+                                    <Col span={24}>
+                                        <Card className='content' bordered={true}>
+                                            {/* mission statement */}
+                                            <div id='mission-statement'>
+                                                <h1 className='display-5 text-white text-capitalize'>Mission
+                                                    Statement</h1>
+                                                <Divider/>
+                                                <p className='lead text-white-50 fw-normal text-left'>
+                                                    Mellow Slothz is here to provide art when available, value when
+                                                    possible,
+                                                    and opportunity where it can be utilized. We aim to always be fair
+                                                    and as
+                                                    open as possible when the community needs us to be so that Mellow
+                                                    Slothz is
+                                                    considered a respected member within the Cardano NFT community. We
+                                                    strive to
+                                                    support our small stake pool operators as much as we are able in
+                                                    order to
+                                                    promote healthy growth and decentralization on the network.
+                                                    <br/>
+                                                    <br/>
+                                                    Mellow Slothz is also committed to doing what we are able to with
+                                                    respect to
+                                                    the vulnerable sloth population. We will be donating at regular
+                                                    intervals to
+                                                    The Sloth Conservation Foundation in Costa Rica. We will continue to
+                                                    look
+                                                    for new missions to support as funds permit.
+                                                </p>
+                                                <br/>
+                                                <br/>
+                                            </div>
+                                        </Card>
+                                    </Col>
+                                    <Col span={12}>
+                                        <Card className='content' bordered={true}>
+                                            {/* who we donate to */}
+                                            <div id='who-we-donate-to'>
+                                                <h1 className='display-5 text-white text-capitalize'>Who we donate
+                                                    to</h1>
+                                                <Divider/>
+                                                <p className='lead text-white-50 fw-normal text-left'>
+                                                    Mellow Slothz is starting off with donations to <a
+                                                    href='https://slothconservation.org/'
+                                                    target='_blank'>slothconservation.org</a> who are doing great work
+                                                    to
+                                                    conserve and protect the sloth population in tropical areas. Sloths
+                                                    encounter many threats due to human encroachment into their
+                                                    habitable zones.
+                                                    These threats include direct human involvement such as hunting and
+                                                    capture
+                                                    for the illegal pet trade.
+                                                    <br/>
+                                                    <br/>
+                                                    Indirect threats arise from dogs, power transmission lines and
+                                                    distribution
+                                                    sites, and roadways when traversing areas of the rainforest. We urge
+                                                    you to
+                                                    visit The Sloth Conservation Foundation and read about how these
+                                                    threats
+                                                    endanger sloths and what the foundation is doing to protect them in
+                                                    our fast
+                                                    changing world.
+                                                </p>
+                                                <br/>
+                                                <br/>
+                                            </div>
+                                        </Card>
+                                    </Col>
+                                    <Col span={12}>
+                                        <Card className='content' bordered={true}>
+                                            {/* future utility */}
+                                            <div id='future-utility'>
+                                                <h1 className='display-5 text-white text-capitalize'>Future Utility</h1>
+                                                <Divider/>
+                                                <p className='lead text-white-50 fw-normal text-left'>
+                                                    Mellow Slothz NFTs are being released in 5 sets of 3000 units each.
+                                                    Each set
+                                                    will be designated in support of a selected small stake pool
+                                                    organization.
+                                                    These stake pools, or ‘rescues’, will be the holders of the wallets
+                                                    that
+                                                    collect proceeds from the sale of Mellow Slothz. Each stake pool set
+                                                    will
+                                                    include
+                                                    the stake pool’s logo and a unique color background.
+                                                    <br/>
+                                                    <br/>
+                                                    Our goal in this is to spread awareness of small stake pools that
+                                                    need a
+                                                    little more support in the way of delegators in order to be selected
+                                                    for
+                                                    block rewards more consistently.
+                                                    <br/>
+                                                    <br/>
+                                                    In the future, holders of a Mellow Slothz NFT will be eligible for
+                                                    bonuses
+                                                    on <a href='https://winegarden.io/'
+                                                          target='_blank'>WineGarden.io</a>. These
+                                                    bonuses can be increased if the person utilizing <a
+                                                    href='https://winegarden.io/' target='_blank'>WineGarden.io</a> has
+                                                    at least
+                                                    one of each set in their wallet.
+                                                </p>
+                                                <br/>
+                                                <br/>
+                                            </div>
+                                        </Card>
+                                    </Col>
+                                </Row>
 
-
-                                    {/* who we donate to */}
-                                    <div id='who-we-donate-to'>
-                                        <h1 className='display-5 text-white text-capitalize'>Who we donate to</h1>
-                                        <Divider/>
-                                        <p className='lead text-white-50 fw-normal text-left'>
-                                            Mellow Slothz is starting off with donations to <a
-                                            href='https://slothconservation.org/'
-                                            target='_blank'>slothconservation.org</a> who are doing great work to
-                                            conserve and protect the sloth population in tropical areas. Sloths
-                                            encounter many threats due to human encroachment into their habitable zones.
-                                            These threats include direct human involvement such as hunting and capture
-                                            for the illegal pet trade.
-                                            <br/>
-                                            <br/>
-                                            Indirect threats arise from dogs, power transmission lines and distribution
-                                            sites, and roadways when traversing areas of the rainforest. We urge you to
-                                            visit The Sloth Conservation Foundation and read about how these threats
-                                            endanger sloths and what the foundation is doing to protect them in our fast
-                                            changing world.
-                                        </p>
-                                        <br/>
-                                        <br/>
-                                    </div>
-
-                                    {/* future utility */}
-                                    <div id='future-utility'>
-                                        <h1 className='display-5 text-white text-capitalize'>Future Utility</h1>
-                                        <Divider/>
-                                        <p className='lead text-white-50 fw-normal text-left'>
-                                            Mellow Slothz NFTs are being released in 5 sets of 3000 units each. Each set
-                                            will be designated in support of a selected small stake pool organization.
-                                            These stake pools, or ‘rescues’, will be the holders of the wallets that
-                                            collect proceeds from the sale of Mellow Slothz. Each stake pool set will
-                                            include
-                                            the stake pool’s logo and a unique color background.
-                                            <br/>
-                                            <br/>
-                                            Our goal in this is to spread awareness of small stake pools that need a
-                                            little more support in the way of delegators in order to be selected for
-                                            block rewards more consistently.
-                                            <br/>
-                                            <br/>
-                                            In the future, holders of a Mellow Slothz NFT will be eligible for bonuses
-                                            on <a href='https://winegarden.io/' target='_blank'>WineGarden.io</a>. These
-                                            bonuses can be increased if the person utilizing <a
-                                            href='https://winegarden.io/' target='_blank'>WineGarden.io</a> has at least
-                                            one of each set in their wallet.
-                                        </p>
-                                        <br/>
-                                        <br/>
-                                    </div>
-
-                                    {/* categorization info */}
+                                {/* categorization info */}
+                                <Card className='content' bordered={true} style={{height: 'auto'}}>
                                     <div id='mellow-slothz-categories'>
                                         <h1 className='display-5 text-white text-capitalize'>How Mellow Slothz are
                                             categorized</h1>
@@ -149,28 +186,16 @@ export default function informationMain() {
                                         <br/>
                                         <br/>
                                     </div>
-
-                                    <BottomNav/>
-                                    <br/>
-                                    <br/>
-                                    <p className='lead black-50'>
-                                        The foundation does not take donations directly, but instead uses a third party
-                                        to
-                                        convert crypto to cash for their use.
-                                    </p>
-                                </div>
+                                </Card>
+                                <BottomNav/>
+                                <p className='lead black-50'>
+                                    The foundation does not take donations directly, but instead uses a third party
+                                    to convert crypto to cash for their use.
+                                </p>
                             </div>
                         </div>
                     </main>
                 </Content>
-            </Col>
-
-            <Col xs={20} sm={16} md={12} lg={8} xl={4}>
-
-            </Col>
-
-            <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-                Col
             </Col>
         </Row>
     </>)
